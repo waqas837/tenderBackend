8a2329213b9f3e3ef6a93270f9aebece5cc08f50
waqas837 <73779175+waqas837@users.noreply.github.com>
2021-05-27 14:22:55 +0500

    make it better

--- Database/userSchema.js
+++ Database/userSchema.js
@@ -39,59 +39,7 @@ const tenderorbidder = new mongoose.Schema({
     required: true,
   }
 }); 
-// Product schema
-const productSchema = new mongoose.Schema({
-  title:String,
-  price:String,
-  // {} owner will single for the a product
-  owner:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"usersignup"
-  }
-});
-//admin
-const admin = new mongoose.Schema({
-  email: {
-    unique: true,
-    type: String,
-    validate(val) {
-      if (!validator.isEmail(val)) throw new Error("emailWrongPattern");
-    },
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  cpassword: {
-    type: String,
-    required: true,
-  },
-});
-
-// addtocart schema
-const addtocart = new mongoose.Schema({
-  title: String,
-  price: String,
-  description: String,
-});
-// Services schema
-const serviceSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    unique:true
-  },
-
-  price: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-});
+ 
 const usersignup = new mongoose.model("tenderbidder", tenderorbidder);
-const Product = new mongoose.model("productData", productSchema);
-const AdminData = new mongoose.model("adminData", admin);
-const ServiceData = new mongoose.model("serviceData", serviceSchema);
-const cartadd = new mongoose.model("addtocartitems", addtocart);
-module.exports = { usersignup, Product, AdminData, ServiceData,cartadd };
+ 
+module.exports = { usersignup };
--- controller/routes.js
+++ controller/routes.js
@@ -3,42 +3,10 @@ const router = express.Router();
 const {
   singin,
   signup, 
-  getData,
-  deleteUser,
-  udpateUser,
-  findSingleUser,
-  addProduct,
-  getProduct,
-  deleteProduct,findSingleProduct,udpateProduct,AdminSignIn,signupAdmin,
-  addServices,deleteServiceData,findSingleService,getserviceData,updateServiceData,
-  addtocart,getallcartSingle,findSingleProductforadd,cartSingleRemove
 } = require("./userLogics");
 // routes for user account
 
 router.post("/signup", signup);
 router.post("/signin", singin);
-router.get("/getData", getData);
-router.delete("/deleteUser/:id", deleteUser);
-router.put("/udpateUser/:id", udpateUser);
-router.get("/findSingleUser/:id", findSingleUser);
-// routes for the productPanel
-router.post("/addProduct", addProduct);
-router.get("/getProduct", getProduct);
-router.delete("/deleteProduct/:id", deleteProduct);
-router.get("/findSingleProduct/:id", findSingleProduct);
-router.put("/udpateProduct/:id", udpateProduct);
-// routes for admin account
-router.post("/signupadmin", signupAdmin);
-router.post("/signinadmin", AdminSignIn);
-// routes for the admin services 
-router.post("/addservice", addServices);
-router.get("/getServices", getserviceData);
-router.delete("/deleteservice/:id", deleteServiceData);
-router.get("/findSingleservice/:id", findSingleService);
-router.put("/udpateservice/:id", updateServiceData);
-// routes for the addtocart user
-router.post("/findSingleProductforadd/:id",findSingleProductforadd);
-router.post("/addtocartSingle",addtocart);
-router.get("/getallcartSingle",getallcartSingle);
-router.delete("/cartSingleRemove/:id",cartSingleRemove);
+ 
 module.exports = router;
